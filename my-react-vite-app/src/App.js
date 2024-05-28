13139dcf5e3217721bcd451bc0dcd4d74d2c7d14
RichardEphraim <makindeayomide2024@gmail.com>
2024-05-28 19:23:44 +0100

    yoo

---
+++ my-react-vite-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css'
+import navbar from './components/navbar/navbar'
+function App() {
+
+  return (
+
+      <div>
+       <navbar/>
+      </div>
+    
+      
+      
+  ); 
+}
+
+export default App
